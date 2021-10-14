fc7194c307b8b8b10dcc7d2b4c2935db5e9a8ba8
Ameen Noushad <86822695+Ameen-Here@users.noreply.github.com>
2021-10-15 00:17:10 +0530

    Day 16 CSS update
    Updated CSS functionality by changing the color when wins. And properly finished reset function.

--- Day15_Guess_My_Number/script.js
+++ Day15_Guess_My_Number/script.js
@@ -10,25 +10,35 @@ const printNumber = function () {
   console.log(guessValue);
 
   if (!guessValue) {
+    //  When there is no value
     prompt("Enter a number before guessing");
     document.querySelector(".message").textContent =
       "You have not selected any value.";
   } else if (guessValue === number) {
+    //  When the user won
     document.querySelector(".message").textContent =
       "You got the correct value.";
     document.querySelector(".number").textContent = guessValue;
-    document.querySelector(".highscore").textContent = score;
+    if (score > highScore) {
+      document.querySelector(".highscore").textContent = score;
+      highScore = score;
+    }
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
   } else if (guessValue > number) {
+    //  When the value is high
     document.querySelector(".message").textContent =
       "You got the wrong value.It's too high";
     score -= 1;
     document.querySelector(".score").textContent = score;
   } else if (guessValue < number) {
+    //  When the value is less
     document.querySelector(".message").textContent =
       "You got the wrong value.It's too low";
     score -= 1;
     document.querySelector(".score").textContent = score;
   } else {
+    //  When the user lost
     document.querySelector(".message").textContent = "You loose the game";
     reset();
   }
@@ -38,6 +48,9 @@ const reset = function () {
   score = 20;
   document.querySelector(".score").textContent = score;
   number = Math.trunc(Math.random() * 10);
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".number").textContent = "?";
 };
 
 document.querySelector(".again").addEventListener("click", reset);
@@ -49,3 +62,4 @@ document.querySelector(".check").addEventListener("click", printNumber);
 // document.querySelector(".check").addEventListener("click", function(){
 //     console.log(document.querySelector(".guess").value);
 // })
+
