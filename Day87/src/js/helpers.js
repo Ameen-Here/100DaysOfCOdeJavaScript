5feb13129c9fba92f561f5261a76a9e07e5f3dee
Ameen Noushad <86822695+Ameen-Here@users.noreply.github.com>
2022-05-19 10:17:42 +0530

    Day 87
    Added configuration and helper module, defined outside of MVC architecture.

---
+++ Day87/src/js/helpers.js
@@ -0,0 +1,23 @@
+import { async } from "regenerator-runtime";
+import { TIMEOUT_SEC } from "./config.js";
+
+const timeout = function (s) {
+  return new Promise((_, reject) => {
+    setTimeout(function () {
+      reject(
+        new Error(`Request took too long to load. Time out after ${s} seconds`)
+      );
+    }, s * 1000);
+  });
+};
+
+export const getJSON = async function (url) {
+  try {
+    const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
+    const datas = await res.json();
+    if (!res.ok) throw new Error(`${datas.message} Status (${res.status})`);
+    return datas;
+  } catch (err) {
+    throw err;
+  }
+};
